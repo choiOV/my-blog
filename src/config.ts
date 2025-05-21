301ecc8d775a29ccd9c0a05fef7fe9b491533e16
choiOV <121269364+choiOV@users.noreply.github.com>
2025-05-21 22:35:03 +0900

    initial commit

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "", // replace this with your deployed domain
+  author: "choiOV",
+  profile: "url",
+  desc: "",
+  title: "choiOV.blog",
+  ogImage: "",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
@@ -13,10 +13,10 @@ export const SITE = {
   showBackButton: true, // show back button in post detail
   editPost: {
     enabled: true,
-    text: "Suggest Changes",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    text: "수정 제안하기",
+    url: "",
   },
   dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Asia/Tokyo", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
---
+++ src/data/blog/blog1.md
@@ -0,0 +1,14 @@
+---
+author: Sat Naing
+pubDatetime: 2025-05-21T13:00:00Z
+modDatetime: 2025-05-21T13:00:00Z
+title: 새 글
+slug: 새 글
+featured: false
+draft: false
+tags:
+  - 게시글
+description: 설명
+---
+
+본문
